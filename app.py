3ac47e998d4f6f65fc5f44d47561a326d6a4193e
Andrew_7_1 <andrew71@villagerrights.xyz>
2022-06-25 21:54:33 +0300

    Start working on docs

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template
 
-from api_blueprint import server_api, update_java_thread
+#from api_blueprint import server_api, update_java_thread
 
 app = Flask(__name__)
 
 # Import server_api and register it as blueprint
-app.register_blueprint(server_api)
+#app.register_blueprint(server_api)
 
 
 @app.route('/')
 def main():
-    return render_template('base.html')
+    return render_template('docs.html')
 
 
 if __name__ == '__main__':
-    update_java_thread()  # Start thread
+    #update_java_thread()  # Start thread
     app.run()
